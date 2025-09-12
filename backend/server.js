537863dffc6bc81bf3d072cf1c9a027adcb0e4cf
Baheti Devkumar Gopal <baheti.gopal23b@iiitg.ac.in>
2025-09-13 00:09:06 +0530

    backend cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const Audio = require("./models/Audio");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: "https://eleven-labs-clone-nine.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json());
 
 mongoose
